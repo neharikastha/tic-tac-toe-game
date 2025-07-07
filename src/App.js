5b2dcbc3febca8ffd6afdf0c342139dbde7dab7d
Neharika Shrestha <neharikas@infinite.com>
2025-07-07 22:41:35 +0545

    feat : UI and some functionalities

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './components/ticTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
